2139cb96733962846043b2d7d1b16a127224b926
Ross Dougherty <rossd987@gmail.com>
2014-03-24 21:11:48 -0600

    another test

---
+++ LetterStatsTests/LetterStatsTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using LetterStats;
+
+namespace LetterStatsTests
+{
+    [TestClass]
+    public class LetterStatsTest
+    {
+        [TestMethod]
+        public void SingleLetter()
+        {
+            var word = "a";
+            var collection = new LetterStatCollection(word);
+
+            Assert.AreEqual("a", collection['a'].LongestWord);
+            Assert.AreEqual("a", collection['a'].ShortestWord);
+            Assert.AreEqual(1, collection['a'].BeginWith);
+            Assert.AreEqual(1, collection['a'].EndWith) ;
+            Assert.AreEqual(1, collection['a'].AverageLength);
+        }
+
+        [TestMethod]
+        public void SingleWord()
+        {
+            var word = "aa";
+            var collection = new LetterStatCollection(word);
+
+            Assert.AreEqual("aa", collection['a'].LongestWord);
+            Assert.AreEqual("aa", collection['a'].ShortestWord);
+            Assert.AreEqual(1, collection['a'].BeginWith);
+            Assert.AreEqual(1, collection['a'].EndWith);
+            Assert.AreEqual(2, collection['a'].AverageLength);
+        }
+        
+        [TestMethod]
+        public void ATest()
+        {
+            string[] words = { "aa", "aah", "aahed", "aahing", "aahs", "aal", "aalii",
+                               "aaliis", "aals", "aardvark", "aardvarks", "aardwolf",
+                               "aardwolves", "aargh", "aarrgh", "aarrghh", "aas", 
+                               "aasvogel", "aasvogels" };
+
+            var collection = new LetterStatCollection(words);
+
+            Assert.AreEqual("aardwolves", collection['a'].LongestWord);
+            Assert.AreEqual("aa", collection['a'].ShortestWord);
+            Assert.AreEqual(19, collection['a'].BeginWith);
+            Assert.AreEqual(1, collection['a'].EndWith);
+            Assert.AreEqual(5.84, collection['a'].AverageLength);
+        }
+    }
+}
---
+++ WordStats/LetterStat.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetterStats
+{
+    public class LetterStat
+    {
+        public LetterStat(char letter)
+            
+        {
+            this.Letter = letter;
+            this.BeginWith = 0;
+            this.EndWith = 0;
+            this.TotalLength = 0;
+            this.WordCount = 0;
+            this.ShortestWord = "";
+            this.LongestWord = "";
+        }
+
+        public LetterStat(char letter, string word)
+        {
+            if (!string.IsNullOrEmpty(word) && word[0] != letter)
+            {
+                throw new ArgumentException("First letter of word does not match letter.");
+            }
+
+            this.Letter = letter;
+            this.ShortestWord = word;
+            this.LongestWord = word;
+            this.WordCount = 1;
+            this.TotalLength = word.Length;
+            this.BeginWith = 1;
+        }
+
+        public char Letter { get; private set; }
+        public int BeginWith { get; set; }
+        public int EndWith { get; set; }
+        public string ShortestWord { get; set; }
+        public string LongestWord { get; set; }
+        public int TotalLength { get; set; }
+        public int WordCount { get; set; }
+        public double AverageLength
+        {
+            get { return Math.Round((double)TotalLength / WordCount, 2); }
+        }
+    }
+}
